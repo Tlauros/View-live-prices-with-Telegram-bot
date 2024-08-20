fe2093acdcc4f4b538a087a1be633c4e570d28b7
Tlauros Dev <105284032+Tlauros@users.noreply.github.com>
2024-08-20 15:57:18 +0330

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+token = '7366062432:AAFjX2Q6y5CPWc6QI9VpMsDz3T9uHqsar_U'
+# Just Work on Public
+CHANNEL_ID = "@showpricedev"
+GROUP_ID = '@testdegroup'
+NameBot = 'example' # You Can Customise name bot on start for example : Hi my friend , welcome to Show Price Live example (crypto/forex)
+YOUR_USER_ID = "NYB_ELT"  # Without @
