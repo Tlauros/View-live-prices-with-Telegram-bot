2b4454e3443a31fdc7d51a52399970a644e5e3e3
Tlauros Dev <105284032+Tlauros@users.noreply.github.com>
2024-08-20 22:56:34 +0330

    allow for edit

---
+++ config.py
@@ -0,0 +1,8 @@
+token = 'YOUR_API_TELEGRAM_BOT'
+# Just Work on Public
+CHANNEL_ID = "ID_WITH_@"
+GROUP_ID = 'ID_WITH_@'
+NameBot = 'example' # You Can Customise name bot on start for example : Hi my friend , welcome to Show Price Live example (crypto/forex)
+YOUR_USER_ID = "Your_User_telegram"  # Without @
+
+SET_TIME_FOR_UPDATE_PRICE = 600 # Sec   (600s ===> 10 Min) Prices are updated every 10 minutes
